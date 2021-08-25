77b26ef986d12e0c8e77723f04de481151d2aeee
MahmoudSamy1452 <86854027+MahmoudSamy1452@users.noreply.github.com>
2021-08-25 16:17:00 +0200

    Configuring Heroku

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+const bodyParser = require('body-parser');
+const cors = require('cors');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = process.env.PORT || 3000;
+const server = app.listen(port, activeServer)
+
+function activeServer(){
+	console.log(`running on localhost:${port}`);
+}
+
+app.get('/getData',dataOut);
+function dataOut(req,res){
+	res.send(projectData);
+	return projectData;
+}
+
+app.post('/postData',dataIn);
+function dataIn(req,res){
+	projectData.push(req.body);
+	console.log(projectData);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,69 @@
 /* Global Variables */
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = 'd5b137fecf844da9a39d78158e1f4806';
+const generateButton = document.querySelector('#generate');
+const zipCode = document.querySelector('#zip');
+const feeling = document.querySelector('#feelings');
+const date = document.querySelector('#date');
+const temp = document.querySelector('#temp');
+const content = document.querySelector('#content');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const getDataFromAPI = async (url) => {
+		const response = await fetch(url)
+
+		try{
+			const data = await response.json();
+			console.log(data);
+			return data;
+		}
+		catch(error){
+			console.log(Error(error));
+		}
+	}
+
+const postDataToApp = async(url,data) =>{
+	const response = await fetch(url,{
+		method: "POST",
+		credentials: "same-origin",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data)
+	})
+	try{
+		const data = await response.json();
+		return data;
+	}
+	catch(error){
+		console.log(Error(error));
+	}
+}
+
+const updateUI = async(url) => {
+	const response = await fetch(url)
+	try{
+		const data = await response.json();
+		console.log(data);
+		date.innerText = data[data.length - 1]["date"];
+		temp.innerText = data[data.length - 1]["temp"];
+		content.innerText = data[data.length - 1]["user-response"];
+	}
+	catch(error){
+		console.log(Error(error));
+	}
+}
+
+function startAction(){
+	const zip = zipCode.value;
+	const userResponse = feeling.value;
+	getDataFromAPI(baseURL+zip+'&appid='+apiKey)
+ .then((response) => {postDataToApp('/postData',{"date" : newDate, "temp" : response.main.temp, "user-response" : userResponse})})
+ .then(() => (updateUI('/getData')))
+}
+	
+
+	generateButton.addEventListener('click', startAction)
